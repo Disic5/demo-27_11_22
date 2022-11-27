3a4fd021cb21d4ae58624eab5aa3187e6c5520e9
Denis <svd.disic5@mail.ru>
2022-11-27 16:56:29 +0300

    Initial commit

---
+++ src/test/java/com/example/demo27_11_22/Demo271122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo27_11_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo271122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
